a4472810db3ee58f1429f16ad91db5701e29d091
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 16:21:41 -0500

    got the login pages done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,17 +5,26 @@ get '/' do
 end
 
 get '/success' do
+  
 end
 
 post '/create' do
   @user = User.new(params[:user])
   if @user.valid?
     @user.save
+    session[:user] = @user.id
     redirect to('/success')
   else
-    redirect to('/')
+    erb :index
   end
 end
 
 post '/login' do
+  @user = User.authenticate(params[:user][:email], params[:user][:raw_password])
+  if @user
+    session[:user] = @user.id
+    redirect to('/success')
+  else
+    erb :index
+  end
 end
